088c7a7dffa60bb7e262406a3c4a764266d85243
chrisdennett <chrisdennett@gmail.com>
2020-04-04 18:36:50 +0100

    Added head

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { saveAs } from "file-saver";
+import { useAnimationFrame } from "./hooks/useAnimationFrame";
 
 const cellSize = 50;
 const width = 600;
@@ -14,6 +15,14 @@ export default () => {
   const [centepedePaths, setCentepedePaths] = React.useState(
     getCentipedePath(gridData)
   );
+  const [count, setCount] = React.useState(0);
+
+  // useAnimationFrame((deltaTime) => {
+  //   // Pass on a function to the setter of the state
+  //   // to make sure we always have the latest state
+  //   setCount((prevCount) => prevCount + 1);
+  //   regenerate();
+  // });
 
   const regenerate = () => {
     const freshGrid = getGridData({ cellSize, width, height });
@@ -23,6 +32,7 @@ export default () => {
 
   return (
     <div>
+      {count}
       <button onClick={save_as_svg}>Save SVG</button>
       <button onClick={regenerate}>Regenerate</button>
       <svg
@@ -37,7 +47,7 @@ export default () => {
       >
         {gridData.map((cell, index) => (
           <rect
-            key={index}
+            key={"grid" + index}
             x={cell.x}
             y={cell.y}
             width={cellSize}
@@ -52,9 +62,43 @@ export default () => {
   );
 };
 
+const getCentipedePath = (cells) => {
+  const paths = [];
+  let count = 0;
+  let endReached = false;
+  let nextCellIndex = getRandomIntBetween(0, cells.length - 1);
+  let prevCellEndedOn;
+
+  while (endReached === false && count < 3000) {
+    const cell = cells[nextCellIndex];
+    const randomNeighBourOption = getAvailableNeighbourOptions(cell, cells);
+
+    if (randomNeighBourOption === null) {
+      endReached = true;
+      break;
+    } else {
+      const option = randomNeighBourOption[0];
+      cell.endedOn = option;
+      nextCellIndex = randomNeighBourOption[1];
+
+      const segment = drawCurrCellPath({
+        cell,
+        option,
+        prevCellEndedOn,
+      });
+
+      paths.push(segment);
+
+      prevCellEndedOn = option;
+    }
+
+    count++;
+  }
+
+  return paths;
+};
+
 const getAvailableNeighbourOptions = (cell, cells) => {
-  // get index above, below, left and right
-  // return available array of "left", "right", "Top", "bottom"
   const { indexAbove, indexBelow, indexLeft, indexRight } = cell;
 
   const possOptions = [];
@@ -62,25 +106,25 @@ const getAvailableNeighbourOptions = (cell, cells) => {
   if (indexAbove >= 0 && !cells[indexAbove].endedOn) {
     possOptions.push(["top", indexAbove]);
   } else {
-    console.log("GET NEIGHBOUR", "top", indexAbove, cells[indexAbove]);
+    // console.log("GET NEIGHBOUR", "top", indexAbove, cells[indexAbove]);
   }
 
   if (indexBelow >= 0 && !cells[indexBelow].endedOn) {
     possOptions.push(["bottom", indexBelow]);
   } else {
-    console.log("GET NEIGHBOUR", "bottom", indexBelow, cells[indexBelow]);
+    // console.log("GET NEIGHBOUR", "bottom", indexBelow, cells[indexBelow]);
   }
 
   if (indexLeft >= 0 && !cells[indexLeft].endedOn) {
     possOptions.push(["left", indexLeft]);
   } else {
-    console.log("GET NEIGHBOUR", "left", indexLeft, cells[indexLeft]);
+    // console.log("GET NEIGHBOUR", "left", indexLeft, cells[indexLeft]);
   }
 
   if (indexRight >= 0 && !cells[indexRight].endedOn) {
     possOptions.push(["right", indexRight]);
   } else {
-    console.log("GET NEIGHBOUR", "right", indexRight, cells[indexRight]);
+    // console.log("GET NEIGHBOUR", "right", indexRight, cells[indexRight]);
   }
 
   if (possOptions.length === 0) return null;
@@ -93,142 +137,134 @@ const getAvailableNeighbourOptions = (cell, cells) => {
 };
 
 const drawCurrCellPath = ({ cell, option, prevCellEndedOn }) => {
-  let centepedePath = "";
+  let path;
+  if (!option) return null;
 
-  if (option) {
-    if (prevCellEndedOn === "right") {
-      if (option === "right") {
-        centepedePath += drawLeftToRight(cell);
-      }
-      if (option === "bottom") {
-        centepedePath += drawLeftToBottom(cell);
-      }
-      if (option === "top") {
-        centepedePath += drawLeftToTop(cell);
-      }
+  // HEAD SEGMENT
+  if (!prevCellEndedOn) {
+    if (option === "right") {
+      path = drawStartToRight(cell);
     }
 
-    if (prevCellEndedOn === "bottom") {
-      if (option === "right") {
-        centepedePath += drawTopToRight(cell);
-      }
-      if (option === "bottom") {
-        centepedePath += drawTopToBottom(cell);
-      }
-      if (option === "left") {
-        centepedePath += drawTopToLeft(cell);
-      }
+    if (option === "left") {
+      path = drawStartToLeft(cell);
+    }
+    if (option === "bottom") {
+      path = drawStartToBottom(cell);
+    }
+    if (option === "top") {
+      path = drawStartToTop(cell);
     }
 
-    if (prevCellEndedOn === "top") {
-      if (option === "right") {
-        centepedePath += drawBottomToRight(cell);
-      }
-      if (option === "top") {
-        centepedePath += drawBottomToTop(cell);
-      }
-      if (option === "left") {
-        centepedePath += drawBottomToLeft(cell);
-      }
-    }
+    return drawHead(cell, path, cell.index);
+  }
 
-    if (prevCellEndedOn === "left") {
-      if (option === "left") {
-        centepedePath += drawRightToLeft(cell);
-      }
-      if (option === "bottom") {
-        centepedePath += drawRightToBottom(cell);
-      }
-      if (option === "top") {
-        centepedePath += drawRightToTop(cell);
-      }
+  // MIDDLE SEGMENTS
+  if (prevCellEndedOn === "right") {
+    if (option === "right") {
+      path = drawLeftToRight(cell);
+    }
+    if (option === "bottom") {
+      path = drawLeftToBottom(cell);
+    }
+    if (option === "top") {
+      path = drawLeftToTop(cell);
     }
   }
 
-  return centepedePath;
-};
-
-const getCentipedePath = (cells) => {
-  const paths = [];
-
-  let firstCellIndex = 0;
-  let prevCellEndedOn = "right";
-  let cell = cells[firstCellIndex];
-  let randomNeighBourOption = getAvailableNeighbourOptions(cell, cells);
-
-  if (!randomNeighBourOption) return "";
-
-  let option = randomNeighBourOption[0];
-  cell.endedOn = option;
-  let nextCellIndex = randomNeighBourOption[1];
-
-  let d = drawCurrCellPath({
-    cell,
-    option,
-    isStart: true,
-    prevCellEndedOn,
-  });
-  prevCellEndedOn = option;
-  paths.push(<path d={d} key={paths.length} />);
-
-  // Two
-  let count = 0;
-  let endReached = false;
-
-  while (endReached === false && count < 3000) {
-    cell = cells[nextCellIndex];
-    randomNeighBourOption = getAvailableNeighbourOptions(cell, cells);
-
-    if (randomNeighBourOption === null) {
-      endReached = true;
-      break;
-    } else {
-      option = randomNeighBourOption[0];
-      cell.endedOn = option;
-      nextCellIndex = randomNeighBourOption[1];
-
-      const d = drawCurrCellPath({
-        cell,
-        option,
-        isStart: true,
-        prevCellEndedOn,
-      });
-
-      paths.push(<path d={d} key={paths.length} />);
-
-      prevCellEndedOn = option;
+  if (prevCellEndedOn === "bottom") {
+    if (option === "right") {
+      path = drawTopToRight(cell);
     }
-
-    count++;
+    if (option === "bottom") {
+      path = drawTopToBottom(cell);
+    }
+    if (option === "left") {
+      path = drawTopToLeft(cell);
+    }
   }
 
-  return paths;
+  if (prevCellEndedOn === "top") {
+    if (option === "right") {
+      path = drawBottomToRight(cell);
+    }
+    if (option === "top") {
+      path = drawBottomToTop(cell);
+    }
+    if (option === "left") {
+      path = drawBottomToLeft(cell);
+    }
+  }
+
+  if (prevCellEndedOn === "left") {
+    if (option === "left") {
+      path = drawRightToLeft(cell);
+    }
+    if (option === "bottom") {
+      path = drawRightToBottom(cell);
+    }
+    if (option === "top") {
+      path = drawRightToTop(cell);
+    }
+  }
+
+  return drawMiddle(path, cell.index);
 };
 
 // DRAW START
-const drawStartInCell = (cell, drawFunction, key) => {
-  const d = drawFunction(cell, true);
-  return [
-    <path d={d} key={key} />,
-    <circle cx={cell.middleX} cy={cell.middleY} r={10} />,
-    <circle cx={cell.middleX} cy={cell.middleY} r={2} />,
-  ];
+const drawHead = (cell, path, key) => {
+  return (
+    <g key={key}>
+      <path d={path} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={10} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />,
+    </g>
+  );
 };
 
 // DRAW END
-const drawEndInCell = (cell, drawFunction, key) => {
-  const d = drawFunction(cell, true);
-  return [
-    <path d={d} key={key} />,
-    <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
-    <circle cx={cell.middleX} cy={cell.middleY} r={2} />,
-  ];
+const drawTail = (cell, path, key) => {
+  return (
+    <g key={key}>
+      <path d={path} key={key} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />,
+    </g>
+  );
 };
 
 // DRAW MIDDLE
-const drawPartInCell = (cell, drawFunction, key) => {
-  const d = drawFunction(cell, true);
-  return <path d={d} key={key} />;
+const drawMiddle = (path, key) => {
+  return <path d={path} key={key} />;
+};
+
+// STARTS
+const drawStartToRight = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.rightMiddle} `;
+  return p;
+};
+const drawStartToLeft = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.leftMiddle} `;
+  return p;
+};
+const drawStartToTop = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.topMiddle} `;
+  return p;
+};
+const drawStartToBottom = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.bottomMiddle} `;
+  return p;
+};
+
+// ENDS
+const drawEndFromTop = (cell) => {
+  let p = `M ${cell.topMiddle} `;
+  p += ` L ${cell.middleMiddle} `;
+  return p;
 };
 
 // From LEFT
@@ -317,15 +353,20 @@ const getGridData = ({ cellSize, width, height }) => {
       const indexRight = xIndex === totalCols - 1 ? -1 : index + 1;
       const indexAbove = yIndex === 0 ? -1 : index - totalCols;
       const indexBelow = yIndex === totalRows - 1 ? -1 : index + totalCols;
+      const middleX = x + halfCell;
+      const middleY = y + halfCell;
 
       cells.push({
         x,
         y,
         index,
+        middleX,
+        middleY,
         indexLeft,
         indexRight,
         indexAbove,
         indexBelow,
+        middleMiddle: `${x + halfCell},${y + halfCell}`,
         topMiddle: `${x + halfCell},${y}`,
         bottomMiddle: `${x + halfCell},${y + cellSize}`,
         leftMiddle: `${x},${y + halfCell}`,
