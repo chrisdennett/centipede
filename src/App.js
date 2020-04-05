3783c3b32c037420fe2ce2eecf5ecc011853098c
chrisdennett <chrisdennett@gmail.com>
2020-04-05 21:31:22 +0100

    Added leg blobs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { saveAs } from "file-saver";
 import { useInterval } from "./hooks/useInterval";
+import Centipede from "./Centipede";
 
 export default () => {
   const [doTimer, setDoTimer] = useState(false);
+  const [inTestMode, setInTestMode] = useState(true);
   const [strokeColour, setStrokeColour] = useState("#ccc");
   const cellSize = 50;
-  const width = 600;
-  const height = 600;
+  const width = 500;
+  const height = 500;
   const svgWidth = width;
   const svgHeight = height;
+
+  const toggleTestMode = () => setInTestMode((prev) => !prev);
 
   useInterval(() => {
     if (doTimer) {
@@ -21,14 +25,20 @@ export default () => {
     setStrokeColour((prev) => (prev === "#ccc" ? "#cccccd" : "#ccc"));
   };
 
-  const gridData = getGridData({ cellSize, width, height });
-  const centepedePaths = getCentipedePath(gridData);
+  const cells = getGridData({ cellSize, width, height });
 
   return (
     <div>
       <button onClick={save_as_svg}>Save SVG</button>
-      <button onClick={regenerate}>Regenerate</button>
-      <button onClick={() => setDoTimer((prev) => !prev)}>Timer</button>
+      <button onClick={toggleTestMode}>
+        Switch Test Mode {inTestMode ? "OFF" : "ON"}
+      </button>
+      {!inTestMode && (
+        <>
+          <button onClick={regenerate}>Regenerate</button>
+          <button onClick={() => setDoTimer((prev) => !prev)}>Timer</button>
+        </>
+      )}
       <svg
         id="svg"
         xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +49,7 @@ export default () => {
         stroke={"red"}
         strokeWidth={2}
       >
-        {gridData.map((cell, index) => (
+        {cells.map((cell, index) => (
           <rect
             key={"grid" + index}
             x={cell.x}
@@ -50,313 +60,10 @@ export default () => {
           />
         ))}
 
-        {centepedePaths}
+        <Centipede cells={cells} inTestMode={inTestMode} />
       </svg>
     </div>
   );
-};
-
-const getCentipedePath = (cells) => {
-  const paths = [];
-  let count = 0;
-  let endReached = false;
-  let nextCellIndex = getRandomIntBetween(0, cells.length - 1);
-  let prevCellEndedOn;
-
-  while (endReached === false && count < 3000) {
-    const cell = cells[nextCellIndex];
-    const randomNeighBourOption = getAvailableNeighbourOptions(cell, cells);
-
-    if (randomNeighBourOption === null) {
-      endReached = true;
-    }
-    const option = randomNeighBourOption ? randomNeighBourOption[0] : null;
-    cell.endedOn = option;
-    nextCellIndex = randomNeighBourOption ? randomNeighBourOption[1] : null;
-
-    const segment = drawCurrCellPath({
-      cell,
-      option,
-      prevCellEndedOn,
-    });
-
-    paths.push(segment);
-
-    prevCellEndedOn = option;
-
-    count++;
-  }
-
-  return paths;
-};
-
-const getAvailableNeighbourOptions = (cell, cells) => {
-  const { indexAbove, indexBelow, indexLeft, indexRight } = cell;
-
-  const possOptions = [];
-
-  if (indexAbove >= 0 && !cells[indexAbove].endedOn) {
-    possOptions.push(["top", indexAbove]);
-  } else {
-    console.log("GET NEIGHBOUR", "top", indexAbove, cells[indexAbove], cell);
-  }
-
-  if (indexBelow >= 0 && !cells[indexBelow].endedOn) {
-    possOptions.push(["bottom", indexBelow]);
-  } else {
-    console.log("GET NEIGHBOUR", "bottom", indexBelow, cells[indexBelow], cell);
-  }
-
-  if (indexLeft >= 0 && !cells[indexLeft].endedOn) {
-    possOptions.push(["left", indexLeft]);
-  } else {
-    console.log("GET NEIGHBOUR", "left", indexLeft, cells[indexLeft], cell);
-  }
-
-  if (indexRight >= 0 && !cells[indexRight].endedOn) {
-    possOptions.push(["right", indexRight]);
-  } else {
-    console.log("GET NEIGHBOUR", "right", indexRight, cells[indexRight], cell);
-  }
-
-  if (possOptions.length === 0) return null;
-
-  // get random option from those available
-  const randIndex = getRandomIntBetween(0, possOptions.length - 1);
-  const randOption = possOptions[randIndex];
-
-  return randOption;
-};
-
-const drawCurrCellPath = ({ cell, option, prevCellEndedOn }) => {
-  let path;
-
-  // HEAD SEGMENT
-  if (!prevCellEndedOn) {
-    if (option === "right") {
-      path = drawStartToRight(cell);
-    }
-    if (option === "left") {
-      path = drawStartToLeft(cell);
-    }
-    if (option === "bottom") {
-      path = drawStartToBottom(cell);
-    }
-    if (option === "top") {
-      path = drawStartToTop(cell);
-    }
-    return drawHead(cell, path, cell.index);
-  }
-
-  // TAIL SEGMENT
-  if (!option) {
-    console.log("ENDED: ", prevCellEndedOn);
-    if (prevCellEndedOn === "right") {
-      path = drawEndFromLeft(cell);
-    }
-    if (prevCellEndedOn === "left") {
-      path = drawEndFromRight(cell);
-    }
-    if (prevCellEndedOn === "bottom") {
-      path = drawEndFromTop(cell);
-    }
-    if (prevCellEndedOn === "top") {
-      path = drawEndFromBottom(cell);
-    }
-    return drawTail(cell, path, cell.index);
-  }
-
-  // MIDDLE SEGMENTS
-  if (prevCellEndedOn === "right") {
-    if (option === "right") {
-      path = drawLeftToRight(cell);
-    }
-    if (option === "bottom") {
-      path = drawLeftToBottom(cell);
-    }
-    if (option === "top") {
-      path = drawLeftToTop(cell);
-    }
-  }
-
-  if (prevCellEndedOn === "bottom") {
-    if (option === "right") {
-      path = drawTopToRight(cell);
-    }
-    if (option === "bottom") {
-      path = drawTopToBottom(cell);
-    }
-    if (option === "left") {
-      path = drawTopToLeft(cell);
-    }
-  }
-
-  if (prevCellEndedOn === "top") {
-    if (option === "right") {
-      path = drawBottomToRight(cell);
-    }
-    if (option === "top") {
-      path = drawBottomToTop(cell);
-    }
-    if (option === "left") {
-      path = drawBottomToLeft(cell);
-    }
-  }
-
-  if (prevCellEndedOn === "left") {
-    if (option === "left") {
-      path = drawRightToLeft(cell);
-    }
-    if (option === "bottom") {
-      path = drawRightToBottom(cell);
-    }
-    if (option === "top") {
-      path = drawRightToTop(cell);
-    }
-  }
-
-  return drawMiddle(path, cell.index);
-};
-
-// DRAW START
-const drawHead = (cell, path, key) => {
-  return (
-    <g key={key}>
-      <path d={path} />,
-      <circle cx={cell.middleX} cy={cell.middleY} r={10} />,
-      <circle cx={cell.middleX} cy={cell.middleY} r={2} />,
-    </g>
-  );
-};
-
-// DRAW END
-const drawTail = (cell, path, key) => {
-  return (
-    <g key={key}>
-      <path d={path} key={key} />,
-      <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
-      <circle cx={cell.middleX} cy={cell.middleY} r={2} />,
-    </g>
-  );
-};
-
-// DRAW MIDDLE
-const drawMiddle = (path, key) => {
-  return <path d={path} key={key} />;
-};
-
-// STARTS
-const drawStartToRight = (cell) => {
-  let p = `M ${cell.middleMiddle} `;
-  p += ` L ${cell.rightMiddle} `;
-  return p;
-};
-const drawStartToLeft = (cell) => {
-  let p = `M ${cell.middleMiddle} `;
-  p += ` L ${cell.leftMiddle} `;
-  return p;
-};
-const drawStartToTop = (cell) => {
-  let p = `M ${cell.middleMiddle} `;
-  p += ` L ${cell.topMiddle} `;
-  return p;
-};
-const drawStartToBottom = (cell) => {
-  let p = `M ${cell.middleMiddle} `;
-  p += ` L ${cell.bottomMiddle} `;
-  return p;
-};
-
-// ENDS
-const drawEndFromTop = (cell) => {
-  let p = `M ${cell.topMiddle} `;
-  p += ` L ${cell.middleMiddle} `;
-  return p;
-};
-const drawEndFromLeft = (cell) => {
-  let p = `M ${cell.leftMiddle} `;
-  p += ` L ${cell.middleMiddle} `;
-  return p;
-};
-const drawEndFromRight = (cell) => {
-  let p = `M ${cell.rightMiddle} `;
-  p += ` L ${cell.middleMiddle} `;
-  return p;
-};
-const drawEndFromBottom = (cell) => {
-  let p = `M ${cell.bottomMiddle} `;
-  p += ` L ${cell.middleMiddle} `;
-  return p;
-};
-
-// From LEFT
-const drawLeftToRight = (cell) => {
-  let p = `M ${cell.leftMiddle} `;
-  p += ` L ${cell.rightMiddle} `;
-  return p;
-};
-
-const drawLeftToBottom = (cell) => {
-  let p = `M ${cell.leftMiddle} `;
-  p += ` L ${cell.bottomMiddle} `;
-  return p;
-};
-
-const drawLeftToTop = (cell) => {
-  let p = `M ${cell.leftMiddle} `;
-  p += ` L ${cell.topMiddle} `;
-  return p;
-};
-
-// FROM TOP
-const drawTopToBottom = (cell) => {
-  let p = `M ${cell.topMiddle} `;
-  p += ` L ${cell.bottomMiddle} `;
-  return p;
-};
-const drawTopToLeft = (cell) => {
-  let p = `M ${cell.topMiddle} `;
-  p += ` L ${cell.leftMiddle} `;
-  return p;
-};
-const drawTopToRight = (cell) => {
-  let p = `M ${cell.topMiddle} `;
-  p += ` L ${cell.rightMiddle} `;
-  return p;
-};
-
-// FROM RIGHT
-const drawRightToBottom = (cell) => {
-  let p = `M ${cell.rightMiddle} `;
-  p += ` L ${cell.bottomMiddle} `;
-  return p;
-};
-const drawRightToLeft = (cell) => {
-  let p = `M ${cell.rightMiddle} `;
-  p += ` L ${cell.leftMiddle} `;
-  return p;
-};
-const drawRightToTop = (cell) => {
-  let p = `M ${cell.rightMiddle} `;
-  p += ` L ${cell.topMiddle} `;
-  return p;
-};
-
-// FROM BOTTOM
-const drawBottomToRight = (cell) => {
-  let p = `M ${cell.bottomMiddle} `;
-  p += ` L ${cell.rightMiddle} `;
-  return p;
-};
-const drawBottomToLeft = (cell) => {
-  let p = `M ${cell.bottomMiddle} `;
-  p += ` L ${cell.leftMiddle} `;
-  return p;
-};
-const drawBottomToTop = (cell) => {
-  let p = `M ${cell.bottomMiddle} `;
-  p += ` L ${cell.topMiddle} `;
-  return p;
 };
 
 const getGridData = ({ cellSize, width, height }) => {
@@ -365,6 +72,8 @@ const getGridData = ({ cellSize, width, height }) => {
   const totalRows = Math.floor(height / cellSize);
   let index = 0;
   const halfCell = cellSize / 2;
+  const thirdCell = cellSize / 3;
+  const twoThirdCell = thirdCell * 2;
 
   for (let yIndex = 0; yIndex < totalRows; yIndex++) {
     for (let xIndex = 0; xIndex < totalCols; xIndex++) {
@@ -377,11 +86,24 @@ const getGridData = ({ cellSize, width, height }) => {
       const indexBelow = yIndex === totalRows - 1 ? -1 : index + totalCols;
       const middleX = x + halfCell;
       const middleY = y + halfCell;
+      const bottomY = y + cellSize;
+      const rightX = x + cellSize;
+      const thirdX = x + thirdCell;
+      const thirdY = y + thirdCell;
+      const twoThirdX = x + twoThirdCell;
+      const twoThirdY = y + twoThirdCell;
 
       cells.push({
         x,
         y,
         index,
+        thirdX,
+        thirdY,
+        twoThirdX,
+        twoThirdY,
+        cellSize,
+        bottomY,
+        rightX,
         middleX,
         middleY,
         indexLeft,
@@ -400,11 +122,6 @@ const getGridData = ({ cellSize, width, height }) => {
   }
 
   return cells;
-};
-
-const getRandomIntBetween = (min, max) => {
-  // return Math.round(min + (max - min) * Math.random());
-  return Math.round(Math.random() * max);
 };
 
 const save_as_svg = () => {
---
+++ src/Centipede.js
@@ -0,0 +1,437 @@
+import React from "react";
+
+const Centipede = ({ cells, inTestMode = true }) => {
+  const segments = inTestMode
+    ? createTestSegments(cells)
+    : createSegments(cells);
+
+  return <>{segments}</>;
+};
+
+export default Centipede;
+
+const createTestSegments = (cells) => {
+  let cellParts = [];
+  cellParts.push(drawSegment({ cell: cells[0], from: "start", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[1], from: "left", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[2], from: "left", to: "bottom" }));
+  cellParts.push(drawSegment({ cell: cells[12], from: "top", to: "left" }));
+  cellParts.push(drawSegment({ cell: cells[11], from: "right", to: "bottom" }));
+  cellParts.push(drawSegment({ cell: cells[21], from: "top", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[22], from: "left", to: "bottom" }));
+  cellParts.push(drawSegment({ cell: cells[32], from: "top", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[33], from: "left", to: "top" }));
+  cellParts.push(drawSegment({ cell: cells[23], from: "bottom", to: "top" }));
+  cellParts.push(drawSegment({ cell: cells[13], from: "bottom", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[14], from: "left", to: "bottom" }));
+  cellParts.push(drawSegment({ cell: cells[24], from: "top", to: "bottom" }));
+  cellParts.push(drawSegment({ cell: cells[34], from: "top", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[35], from: "left", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[36], from: "left", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[37], from: "left", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[38], from: "left", to: "top" }));
+  cellParts.push(drawSegment({ cell: cells[28], from: "bottom", to: "left" }));
+  cellParts.push(drawSegment({ cell: cells[27], from: "left", to: "right" }));
+  cellParts.push(drawSegment({ cell: cells[26], from: "right", to: "top" }));
+  cellParts.push(drawSegment({ cell: cells[16], from: "bottom", to: "left" }));
+  cellParts.push(drawSegment({ cell: cells[15], from: "right", to: "bottom" }));
+  cellParts.push(drawSegment({ cell: cells[25], from: "top", to: "end" }));
+
+  return cellParts;
+};
+
+const createSegments = (cells) => {
+  const paths = [];
+  let count = 0;
+  let endReached = false;
+  let nextCellIndex = getRandomIntBetween(0, cells.length - 1);
+  let prevCellEndedOn;
+
+  while (endReached === false && count < 300000) {
+    const cell = cells[nextCellIndex];
+    const randomNeighBourOption = getAvailableNeighbourOptions(cell, cells);
+
+    if (randomNeighBourOption === null) {
+      endReached = true;
+    }
+    const sideToDrawTo = randomNeighBourOption
+      ? randomNeighBourOption[0]
+      : null;
+    cell.endedOn = sideToDrawTo;
+    nextCellIndex = randomNeighBourOption ? randomNeighBourOption[1] : null;
+
+    const sideToDrawFrom = getStartSideFromEndSide(prevCellEndedOn);
+    const segment = drawSegment({
+      cell,
+      from: sideToDrawFrom ? sideToDrawFrom : "start",
+      to: sideToDrawTo ? sideToDrawTo : "end",
+    });
+
+    paths.push(segment);
+    prevCellEndedOn = sideToDrawTo;
+    count++;
+  }
+
+  return paths;
+};
+
+const getStartSideFromEndSide = (endSide) => {
+  if (endSide === "bottom") return "top";
+  if (endSide === "top") return "bottom";
+  if (endSide === "right") return "left";
+  if (endSide === "left") return "right";
+};
+
+const getAvailableNeighbourOptions = (cell, cells) => {
+  const { indexAbove, indexBelow, indexLeft, indexRight } = cell;
+
+  const possOptions = [];
+
+  if (indexAbove >= 0 && !cells[indexAbove].endedOn) {
+    possOptions.push(["top", indexAbove]);
+  }
+
+  if (indexBelow >= 0 && !cells[indexBelow].endedOn) {
+    possOptions.push(["bottom", indexBelow]);
+  }
+
+  if (indexLeft >= 0 && !cells[indexLeft].endedOn) {
+    possOptions.push(["left", indexLeft]);
+  }
+
+  if (indexRight >= 0 && !cells[indexRight].endedOn) {
+    possOptions.push(["right", indexRight]);
+  }
+
+  if (possOptions.length === 0) return null;
+
+  // get random option from those available
+  const randIndex = getRandomIntBetween(0, possOptions.length - 1);
+  const randOption = possOptions[randIndex];
+
+  return randOption;
+};
+
+// DRAW SEGMENT
+const drawSegment = ({ cell, from, to }) => {
+  let s = null;
+  if (from === "start") {
+    if (to === "right") s = drawStartToRight(cell);
+    if (to === "bottom") s = drawStartToBottom(cell);
+    if (to === "left") s = drawStartToLeft(cell);
+    if (to === "top") s = drawStartToTop(cell);
+  }
+  if (to === "end") {
+    if (from === "top") s = drawTopToEnd(cell);
+    if (from === "left") s = drawLeftToEnd(cell);
+    if (from === "right") s = drawRightToEnd(cell);
+    if (from === "bottom") s = drawBottomToEnd(cell);
+  }
+  if (from === "left") {
+    if (to === "right") s = drawLeftToRight(cell);
+    if (to === "bottom") s = drawLeftToBottom(cell);
+    if (to === "top") s = drawLeftToTop(cell);
+  }
+  if (from === "top") {
+    if (to === "right") s = drawTopToRight(cell);
+    if (to === "bottom") s = drawTopToBottom(cell);
+    if (to === "left") s = drawTopToLeft(cell);
+  }
+  if (from === "bottom") {
+    if (to === "right") s = drawBottomToRight(cell);
+    if (to === "top") s = drawBottomToTop(cell);
+    if (to === "left") s = drawBottomToLeft(cell);
+  }
+  if (from === "right") {
+    if (to === "bottom") s = drawRightToBottom(cell);
+    if (to === "top") s = drawRightToTop(cell);
+    if (to === "left") s = drawRightToLeft(cell);
+  }
+  return s;
+};
+
+// From LEFT
+const drawLeftToRight = (cell) => {
+  let p = `M ${cell.leftMiddle} `;
+  p += ` L ${cell.rightMiddle} `;
+  return <path d={p} key={cell.index} />;
+};
+const drawLeftToBottom = (cell) => {
+  const { x, middleY, middleX, bottomY, thirdX, twoThirdY } = cell;
+
+  const start = { x: x, y: middleY };
+  const cpt1 = { x: thirdX, y: middleY };
+  const cpt2 = { x: middleX, y: twoThirdY };
+  const end = { x: middleX, y: bottomY };
+
+  return getCornerSegment({ start, cpt1, cpt2, end, key: cell.index });
+};
+const drawLeftToTop = (cell) => {
+  const { x, y, middleY, middleX, thirdX, thirdY } = cell;
+
+  const start = { x: x, y: middleY };
+  const cpt1 = { x: thirdX, y: middleY };
+  const cpt2 = { x: middleX, y: thirdY };
+  const end = { x: middleX, y: y };
+
+  return getCornerSegment({
+    start,
+    cpt1,
+    cpt2,
+    end,
+    key: cell.index,
+  });
+};
+
+// FROM TOP
+const drawTopToBottom = (cell) => {
+  let p = `M ${cell.topMiddle} `;
+  p += ` L ${cell.bottomMiddle} `;
+  return <path d={p} key={cell.index} />;
+};
+const drawTopToLeft = (cell) => {
+  const { x, y, middleY, middleX, thirdX, thirdY } = cell;
+
+  const start = { x: middleX, y: y };
+  const cpt1 = { x: middleX, y: thirdY };
+  const cpt2 = { x: thirdX, y: middleY };
+  const end = { x: x, y: middleY };
+
+  return getCornerSegment({
+    start,
+    cpt1,
+    cpt2,
+    end,
+    key: cell.index,
+  });
+};
+const drawTopToRight = (cell) => {
+  const { x, y, middleY, middleX, twoThirdX, thirdY, rightX } = cell;
+
+  const start = { x: middleX, y: y };
+  const cpt1 = { x: middleX, y: thirdY };
+  const cpt2 = { x: twoThirdX, y: middleY };
+  const end = { x: rightX, y: middleY };
+
+  return getCornerSegment({
+    start,
+    cpt1,
+    cpt2,
+    end,
+    key: cell.index,
+  });
+};
+
+// FROM RIGHT
+const drawRightToBottom = (cell) => {
+  const { bottomY, middleY, middleX, twoThirdY, twoThirdX, rightX } = cell;
+
+  const start = { x: rightX, y: middleY };
+  const cpt1 = { x: twoThirdX, y: middleY };
+  const cpt2 = { x: middleX, y: twoThirdY };
+  const end = { x: middleX, y: bottomY };
+
+  return getCornerSegment({
+    start,
+    cpt1,
+    cpt2,
+    end,
+    key: cell.index,
+  });
+};
+const drawRightToLeft = (cell) => {
+  let p = `M ${cell.rightMiddle} `;
+  p += ` L ${cell.leftMiddle} `;
+  return <path d={p} key={cell.index} />;
+};
+const drawRightToTop = (cell) => {
+  const { y, middleY, middleX, thirdY, twoThirdX, rightX } = cell;
+
+  const start = { x: rightX, y: middleY };
+  const cpt1 = { x: twoThirdX, y: middleY };
+  const cpt2 = { x: middleX, y: thirdY };
+  const end = { x: middleX, y: y };
+
+  return getCornerSegment({
+    start,
+    cpt1,
+    cpt2,
+    end,
+    key: cell.index,
+  });
+};
+
+// FROM BOTTOM
+const drawBottomToRight = (cell) => {
+  const { bottomY, middleY, middleX, twoThirdY, twoThirdX, rightX } = cell;
+
+  const start = { x: middleX, y: bottomY };
+  const cpt1 = { x: middleX, y: twoThirdY };
+  const cpt2 = { x: twoThirdX, y: middleY };
+  const end = { x: rightX, y: middleY };
+
+  return getCornerSegment({
+    start,
+    cpt1,
+    cpt2,
+    end,
+    key: cell.index,
+  });
+};
+const drawBottomToLeft = (cell) => {
+  const { x, middleY, middleX, bottomY, thirdX, twoThirdY } = cell;
+
+  const start = { x: middleX, y: bottomY };
+  const cpt1 = { x: middleX, y: twoThirdY };
+  const cpt2 = { x: thirdX, y: middleY };
+  const end = { x: x, y: middleY };
+
+  return getCornerSegment({ start, cpt1, cpt2, end, key: cell.index });
+};
+const drawBottomToTop = (cell) => {
+  let p = `M ${cell.bottomMiddle} `;
+  p += ` L ${cell.topMiddle} `;
+  return <path d={p} key={cell.index} />;
+};
+
+// STARTS
+const drawStartToRight = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.rightMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={10} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+const drawStartToLeft = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.leftMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={10} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+const drawStartToTop = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.topMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={10} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+const drawStartToBottom = (cell) => {
+  let p = `M ${cell.middleMiddle} `;
+  p += ` L ${cell.bottomMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={10} />
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+
+// ENDS
+const drawTopToEnd = (cell) => {
+  let p = `M ${cell.topMiddle} `;
+  p += ` L ${cell.middleMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+const drawLeftToEnd = (cell) => {
+  let p = `M ${cell.leftMiddle} `;
+  p += ` L ${cell.middleMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+const drawRightToEnd = (cell) => {
+  let p = `M ${cell.rightMiddle} `;
+  p += ` L ${cell.middleMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+const drawBottomToEnd = (cell) => {
+  let p = `M ${cell.bottomMiddle} `;
+  p += ` L ${cell.middleMiddle} `;
+  return (
+    <g key={cell.index}>
+      <path d={p} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={5} />,
+      <circle cx={cell.middleX} cy={cell.middleY} r={2} />
+    </g>
+  );
+};
+
+//
+// HELPERS
+//
+const getPointOnCurve = ({ dist = 0.5, start, end, cpt1, cpt2 }) => {
+  const p0 = lerp(start, cpt1, dist);
+  const p1 = lerp(cpt1, cpt2, dist);
+  const p2 = lerp(cpt2, end, dist);
+  const p3 = lerp(p0, p1, dist);
+  const p4 = lerp(p1, p2, dist);
+
+  return lerp(p3, p4, dist);
+};
+
+const lerp = (ptA, ptB, dist) => {
+  return {
+    x: (ptB.x - ptA.x) * dist + ptA.x, // the x coordinate
+    y: (ptB.y - ptA.y) * dist + ptA.y, // the y coordinate
+  };
+};
+
+const getCornerSegment = ({ start, end, cpt1, cpt2, key, showLegs = true }) => {
+  let quad = `M ${start.x},${start.y} `;
+  quad += `C ${cpt1.x},${cpt1.y} `;
+  quad += ` ${cpt2.x},${cpt2.y} `;
+  quad += ` ${end.x},${end.y} `;
+
+  const leg1Base = getPointOnCurve({ dist: 0.25, start, end, cpt1, cpt2 });
+  const leg2Base = getPointOnCurve({ dist: 0.5, start, end, cpt1, cpt2 });
+  const leg3Base = getPointOnCurve({ dist: 0.75, start, end, cpt1, cpt2 });
+
+  return (
+    <g key={key}>
+      <path d={quad} />
+
+      {showLegs && (
+        <>
+          <circle cx={leg1Base.x} cy={leg1Base.y} r={3} />
+          <circle cx={leg2Base.x} cy={leg2Base.y} r={3} />
+          <circle cx={leg3Base.x} cy={leg3Base.y} r={3} />
+        </>
+      )}
+    </g>
+  );
+};
+
+const getRandomIntBetween = (min, max) => {
+  // return Math.round(min + (max - min) * Math.random());
+  return Math.round(Math.random() * max);
+};
